af66a0b3d328099324c12f5c08e8eece7646883e
mdmunir <misbahuldmunir@gmail.com>
2017-06-05 12:58:09 +0700

    fix asset for ng-route

--- assets.php
+++ assets.php
@@ -9,7 +9,7 @@ return[
     'ngMockE2E' => 'https://ajax.googleapis.com/ajax/libs/angularjs/1.6.3/angular-mocks.min.js',
     'ngParseExt' => 'https://ajax.googleapis.com/ajax/libs/angularjs/1.6.3/angular-parse-ext.min.js',
     'ngResource' => 'https://ajax.googleapis.com/ajax/libs/angularjs/1.6.3/angular-resource.min.js',
-    'ngRoute' => 'https://ajax.googleapis.com/ajax/libs/angularjs/1.6.3/angular-sanitize.min.js',
+    'ngRoute' => 'https://ajax.googleapis.com/ajax/libs/angularjs/1.6.3/angular-route.min.js',
     'ngSanitize' => 'https://ajax.googleapis.com/ajax/libs/angularjs/1.6.3/angular-sanitize.min.js',
     'ngTouch' => 'https://ajax.googleapis.com/ajax/libs/angularjs/1.6.3/angular-touch.min.js',
     'ui.bootstrap' => 'https://angular-ui.github.io/bootstrap/ui-bootstrap-tpls-2.5.0.min.js',
